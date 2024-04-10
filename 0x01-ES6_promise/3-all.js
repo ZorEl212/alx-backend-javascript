438332d8a89b76d730004fc827e78bca4388a4ab
ZorEl212 <yabsirad212@gmail.com>
2024-04-11 01:29:49 +0300

    Make some changes on code and fix eslint
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((messages) => {
-      console.log(`${messages[0].body} ${messages[1].firstName} ${messages[1].lastName}`);
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch('Signup system offline');
+    .catch(() => console.log('Signup system offline'));
 }
