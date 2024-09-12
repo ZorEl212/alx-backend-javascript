daabd96880bdb9c687ebc8e82c088e37204a43c6
ZorEl212 <yabsirad212@proton.me>
2024-09-12 19:07:05 -0400

    Fix issues on stdin writing
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- 0x05-Node_JS_basic/0-main.js
+++ 0x05-Node_JS_basic/0-main.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const displayMessage = require('./0-console');
 
 displayMessage("Hello NodeJS!");
--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 process.stdin.setEncoding('utf-8');
-console.log("Welcome to Holberton school, what is your name?");
+process.stdout.write('Please type your name:\n');
 
 process.stdin.on('readable', () => {
 	let chunk = process.stdin.read();
 	if (chunk) {
-		console.log("Your name is: " + chunk);
+		process.stdout.write(`Your name is: ${chunk}`);
 	}
 });
 
