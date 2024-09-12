9e0fe38bc7cb4c2a87d324056945c52bf7bcd785
ZorEl212 <yabsirad212@proton.me>
2024-09-12 19:23:58 -0400

    Refactor stdin.js to improve code readability and consistency
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,12 +3,12 @@ process.stdin.setEncoding('utf-8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-	let chunk = process.stdin.read();
-	if (chunk) {
-		process.stdout.write(`Your name is: ${chunk}`);
-	}
+  const chunk = process.stdin.read();
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
 process.stdin.on('end', () => {
-	console.log("This important software is now closing");
+  console.log('This important software is now closing');
 });
